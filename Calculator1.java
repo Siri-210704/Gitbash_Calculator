f979da5b0e60393bdf9dc2d14537cd556cead131
unknown <01fe22bca213@kletech.ac.in>
2024-02-08 11:58:03 +0530

    Updated code with Square and Cube Functions

--- Calculator1.java
+++ Calculator1.java
@@ -15,6 +15,16 @@ class Calculator1{
     float division(float operand_1, float operand_2){
         return(operand_1 / operand_2);
     }
+
+    float square(float operand_1){
+        return(operand_1 * operand_1);
+    }
+
+    float cube(float operand_1){
+        return(operand_1 * operand_1 * operand_1);
+    }
+
+
     public static void main(String[] args){
         float operand_1 = 10;
         float operand_2 = 5;
@@ -24,13 +34,18 @@ class Calculator1{
         System.out.println("Result of Addition is: " + add_result);
 
         float sub_result = calc.subtraction(operand_1,operand_2);
-        System.out.println("Result of Subtraction is: " + add_result);
+        System.out.println("Result of Subtraction is: " + sub_result);
 
         float mul_result = calc.multiplication(operand_1,operand_2);
-        System.out.println("Result of Multiplication is: " + add_result);
+        System.out.println("Result of Multiplication is: " + mul_result);
 
         float div_result = calc.division(operand_1,operand_2);
-        System.out.println("Result of Division is: " + add_result);
+        System.out.println("Result of Division is: " + div_result);
         
+        float sqr_result = calc.square(operand_1);
+        System.out.println("Result of Square is: " + sqr_result);
+
+        float cube_result = calc.cube(operand_1);
+        System.out.println("Result of cube is: " + cube_result);
     }
 }
