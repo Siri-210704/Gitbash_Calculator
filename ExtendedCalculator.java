249cf53a8a7f645097ff6b01e5e5055956600168
Siri <01fe22bca213@kletech.ac.in>
2024-02-14 16:35:43 +0530

    adding two file to branch

---
+++ ExtendedCalculator.java
@@ -0,0 +1,14 @@
+public class ExtendedCalculator {
+
+    int Triangle (int base , int height)
+    {
+        int area = (1/2 * base * height);
+        return area;
+    }
+
+    public static float Modulas (float operand_1  , float operand_2 )
+    {
+        return operand_1 % operand_2;
+    }
+    
+}
